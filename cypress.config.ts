37f6b11f3899295a919c1adc852af39887f7876b
Pablo Banzo <pridapablo@gmail.com>
2024-02-28 00:45:06 -0600

    config cypress with videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true,
+  screenshotOnRunFailure: true,
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/report/mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
