9bd4b769c54aad18d0b64fc2a585d2e0e5810a3c
Pablo Banzo <pridapablo@gmail.com>
2024-02-27 19:28:36 -0600

    Add Cognito authentication configuration

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dashboardAuth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "agentGroupRole": "string",
+      "supervisorGroupRole": "string"
+    }
+  }
+}
