912cdca778dcd62485479f0915f9f56253c01127
Pablo Banzo <pridapablo@gmail.com>
2024-02-27 19:56:00 -0600

    push cypress tests

---
+++ cypress/e2e/authenticator_spec.cy.ts
@@ -0,0 +1,27 @@
+describe("Authenticator:", function () {
+  // Step 1: setup the application state
+  beforeEach(function () {
+    cy.visit("/");
+  });
+
+  describe("Sign In:", () => {
+    it("allows a user to signin", () => {
+      // Step 2: Take an action (Sign in)
+
+      cy.get(selectors.emailInput).type("pridapablo@gmail.com");
+      cy.get(selectors.signInPasswordInput).type("TestUser123");
+      cy.get(selectors.signInButton).contains("Sign in").click();
+
+      cy.wait(3000);
+
+      // Step 3: Make an assertion (Check for sign-out text)
+      // cy.get(selectors.signOutButton).contains("Sign out");
+    });
+  });
+});
+export const selectors = {
+  emailInput: 'input[type="email"]',
+  signInPasswordInput: 'input[type="password"]',
+  signInButton: 'button[type="submit"]', // or use '.amplify-button--primary.amplify-button--fullwidth' or 'cy.contains('button', 'Sign in')'
+  // signOutButton: 'your-sign-out-button-selector', // Update this according to your actual sign-out button
+};
