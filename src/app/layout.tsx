585e14d456590084c20773a6fae1b848cb251e13
Pablo Banzo <pridapablo@gmail.com>
2024-04-11 19:40:24 -0600

    Add dependencies and update layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import WrappedAuthenticator from "./authenticator";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Wallmart Dashboard",
   description: "For Amazon Connect Supervisors and Agents",
 };
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -14,8 +21,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <WrappedAuthenticator>{children}</WrappedAuthenticator>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {/* <WrappedAuthenticator> */}
+        {children}
+        {/* </WrappedAuthenticator> */}
       </body>
     </html>
   );
