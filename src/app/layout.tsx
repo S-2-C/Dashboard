68d6d3f934cc45c40a14fc7d13fd04ec74dbdf50
luciabarre <lbarrenecheac@gmail.com>
2024-04-11 19:24:42 -0600

    Add global CSS styles and font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
+import "@/styles/globals.css"
+import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import WrappedAuthenticator from "./authenticator";
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Wallmart Dashboard",
@@ -14,8 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
         <WrappedAuthenticator>{children}</WrappedAuthenticator>
+        
       </body>
     </html>
   );
