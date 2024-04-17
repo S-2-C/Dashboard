a5206b1d83db89ac6d17146661b8b600ebd46ff3
Emilia Salazar <emiliasalazar67@gmail.com>
2024-04-16 20:04:37 -0600

    not implemented api rtm

--- src/app/connect/historical/route.ts
+++ src/app/connect/historical/route.ts
@@ -1,3 +1,33 @@
+import { ConnectClient, GetMetricDataV2Command } from "@aws-sdk/client-connect";
+
+async function getData() {
+  if (
+    !process.env.AMAZON_REGION ||
+    !process.env.CONNECT_ACCESS_KEY ||
+    !process.env.CONNECT_SECRET_ACCESS_KEY
+  )
+    throw new Error("NO KEYS");
+  const client = new ConnectClient({
+    region: process.env.AMAZON_REGION,
+    credentials: {
+      accessKeyId: process.env.CONNECT_ACCESS_KEY,
+      secretAccessKey: process.env.CONNECT_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const input = {
+    ResourceArn: process.env.CONNECT_INSTANCE_ARN,
+    StartTime: new Date(),
+    EndTime: new Date(),
+  };
+  // const command = new GetMetricDataV2Command(input);
+  // const response = await client.send(command);
+  // return Response.json(response);
+  return new Response("Not Implementes Yet", {
+    status: 501,
+  });
+}
+
 export async function GET() {
   return new Response("Hello from historic");
 }
