9db80205c75a53e27bf6ea694a8d594e674281a8
daniel-rdcz <danield_895@hotmail.com>
2024-04-19 01:07:22 -0600

    90% GetCurrentMetricData waiting FullStack add agents

--- src/app/connect/GetCurrentMetricData/route.ts
+++ src/app/connect/GetCurrentMetricData/route.ts
@@ -1,3 +1,4 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { ConnectClient, GetCurrentMetricDataCommand, GetCurrentMetricDataCommandInput } from "@aws-sdk/client-connect";
 
 // Configuración del cliente (debes proporcionar esta configuración)
@@ -11,34 +12,44 @@ function makeConfig(): any {
     };
 }
 
-async function getUserMetricData(InstanceId: string, client: ConnectClient): Promise<any> {
+async function getUserMetricData(client: ConnectClient,  InstanceId: string): Promise<any> {
   const input: GetCurrentMetricDataCommandInput = {
     InstanceId: InstanceId,
-    Filters: { // Filters
-      Queues: [ // Queues
-        "fe0bd989-c3d1-4959-a47c-b7d27def9a99",
-      ],
-      Channels: [ // Channels
-        "VOICE" || "CHAT" || "TASK",
-      ],
-    },
-    Groupings: [ // Groupings
-      "QUEUE" || "CHANNEL" || "ROUTING_PROFILE" || "ROUTING_STEP_EXPRESSION",
-    ],
-    CurrentMetrics: [ // CurrentMetrics // required
-      { // CurrentMetric
-        Name: "AGENTS_ONLINE",
-        Unit: "COUNT",
+    CurrentMetrics: [
+      {
+        Name: "AGENTS_AVAILABLE",
+        Unit: "COUNT"
+      },
+      {
+        Name: "AGENTS_ON_CALL",
+        Unit: "COUNT"
       },
     ],
+    Filters: { 
+      Queues: [ 
+        "fe0bd989-c3d1-4959-a47c-b7d27def9a99", "3f2a1212-1458-471f-8adb-04080c44f235", "ab454c37-6f3e-4c50-9b01-e0f262ad5336"
+      ],
+      Channels: [ 
+        "VOICE"
+      ],
+    },
+    Groupings: [ 
+      "QUEUE", "CHANNEL"
+    ],
+    SortCriteria: [
+      {
+        SortByMetric: "AGENTS_AVAILABLE",
+        SortOrder: "ASCENDING"
+      },
+    ]
   };
-
 
     const command = new GetCurrentMetricDataCommand(input);
     const response = await client.send(command);
     return response;
 }
 
+/*
 function arrangeMetricData(response: any): any {
     const metricData = response.MetricResults;
     const metricDataList = metricData.map((metric: any) => {
@@ -49,19 +60,17 @@ function arrangeMetricData(response: any): any {
     });
     return metricDataList;
 }
+*/
 
-export async function GET(): Promise<any> {
-    const config = makeConfig();
-    const InstanceId: string = process.env.CONNECT_INSTANCE_ID || "";
-    const client = new ConnectClient(config as any);
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const config = makeConfig();
+  const InstanceId: string = process.env.CONNECT_INSTANCE_ID || "";
+  const client = new ConnectClient(config as any);
 
-    const response = await getUserMetricData(InstanceId, client);
+  const response = await getUserMetricData(client, InstanceId)
 
-    console.log(response);
+  return Response.json(response);
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(arrangeMetricData(response)),
-    };
+
 }
 
