9a102cdf04f374e4ce33593b387f209b36bb5c62
Pablo Banzo <pridapablo@gmail.com>
2024-04-23 19:38:57 -0600

    impl callStart λ

--- amplify/backend/function/callStart/src/index.js
+++ amplify/backend/function/callStart/src/index.js
@@ -6,18 +6,70 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const REGION = process.env.AWS_REGION;
+const APPSYNCURL = process.env.API_DASHBOARD_GRAPHQLAPIENDPOINTOUTPUT;
+const GQLAPIKEY = process.env.API_DASHBOARD_GRAPHQLAPIKEYOUTPUT;
+const { sendAppSyncRequest } = require("./helpers/appSync.js");
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+// Function for updating a fixture
+async function createContact(phone, id, callStart) {
+  const createContact = {
+    query: `mutation CreateContact($input: CreateContactInput!) {
+        createContact(input: $input) {
+            phone
+            id
+            callStart
+        }
+      }`,
+    variables: {
+      input: {
+        phone,
+        id,
+        callStart,
+      },
+    },
+  };
+  console.log(`Declared createContact with id ${id}`);
+
+  const createContactRes = await sendAppSyncRequest(
+    APPSYNCURL,
+    REGION,
+    "POST",
+    createContact,
+    GQLAPIKEY
+  );
+
+  if (!createContactRes.data[`createContact`]) {
+    console.error(`Could not create contact with id ${id}`);
+  }
+
+  console.log(`Created contact with id ${id}`);
+
+  return createContactRes.data[`createContact`];
+}
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  const contactID = event.Details.Parameters.contactID;
+  const phone = event.Details.ContactData.CustomerEndpoint.Address;
+  const callStart = new Date().toISOString();
+  try {
+    console.log(`Creating contact with id ${contactID}`);
+    const res = await createContact(phone, contactID, callStart);
+    console.log(`Created contact with id ${contactID}`);
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+      statusCode: 200,
+      body: JSON.stringify(res),
     };
+  } catch (error) {
+    console.error(`Error creating contact: ${error}`);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(`Error creating contact: ${error}`),
+    };
+  }
 };
