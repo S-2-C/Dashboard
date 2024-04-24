01ca59929f5f1e1c0ea29db0a5648cbe9e34f5a8
Fernanda Osorio <A01026502@tec.mx>
2024-04-23 18:15:24 -0600

    Refactor ControlPanel Notifications styling

--- src/app/ControlPanel/@Notif/page.tsx
+++ src/app/ControlPanel/@Notif/page.tsx
@@ -4,7 +4,7 @@ export default async function NotifSlot() {
       <h1 className="text-4xl font-bold text-white text-center p-4">
         Notifications
       </h1>
-      <p className="text">
+      {/* <p className="text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -12,16 +12,23 @@ export default async function NotifSlot() {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
-      </p>
+      </p> */}
       <div className="bg-blue p-4 rounded-lg shadow-md mb-4">
         {" "}
         {/* Added mb-4 for margin-bottom */}
-        <text>Notification</text>
+        <text className="font-bold">Notification</text>
       </div>
       <div className="bg-blue p-4 rounded-lg shadow-md mb-4">
         {" "}
         {/* Added mb-4 for margin-bottom */}
-        <text>Notification</text>
+        <text className="font-bold">Notification</text>
+      </div>
+      <div className="bg-figma-figma1 p-4 rounded-lg shadow-md mb-4">
+        {" "}
+        {/* Added mb-4 for margin-bottom */}
+        <text className="text-blue-dark font-bold">Notification Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</text>
       </div>
     </div>
   );
