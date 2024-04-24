d04cd32601f075cc269ebb7641195849f1c44557
Fernanda Osorio <A01026502@tec.mx>
2024-04-23 19:44:27 -0600

    Add AgentBlack.svg and AgentWhite.svg images

--- src/app/ControlPanel/@Agent/page.tsx
+++ src/app/ControlPanel/@Agent/page.tsx
@@ -2,20 +2,22 @@
 
 export default async function AgentSlot() {
   return (
-    <div>
-    <div className="bg-blue-highlight p-4 rounded-lg">
-      <h1 className="text-4xl font-bold text-center p-4">Agent</h1>
-      <p className="text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-        qui officia deserunt mollit anim id est laborum.
-        
-      </p>
-    </div>
+    <div className ="bg-blue-highlight rounded-lg p-8" style={{ display: "inline-block" }}>
+  
+  <div className="bg-blue-highlight p-4 rounded-lg">
+    <h1 className="text-4xl font-bold text-center p-4">Agent</h1>
   </div>
+  <div className="grid grid-cols-5 gap-x-16 gap-y-2 ">
+    {[...Array(20)].map((_, index) => (
+      <div key={index} className="rounded-lg flex flex-col items-center justify-center">
+        <div>
+          <img src="images/AgentBlack.svg" alt="Agent" className="w-10 h-auto mx-auto" />
+          <p className="text-center text-sm">#12DAC</p>
+        </div>
+      </div>
+    ))}
+  </div>
+
+</div>
   );
 }
