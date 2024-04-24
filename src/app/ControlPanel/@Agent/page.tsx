1cf72567c9d77eecb7135f6d58eadb21f36fd388
Fernanda Osorio <A01026502@tec.mx>
2024-04-23 19:54:57 -0600

    Update ControlPanel Agent page styling

--- src/app/ControlPanel/@Agent/page.tsx
+++ src/app/ControlPanel/@Agent/page.tsx
@@ -4,15 +4,21 @@ export default async function AgentSlot() {
   return (
     <div className ="bg-blue-highlight rounded-lg p-8" style={{ display: "inline-block" }}>
   
-  <div className="bg-blue-highlight p-4 rounded-lg">
-    <h1 className="text-4xl font-bold text-center p-4">Agent</h1>
+  <div className="bg-blue-highlight rounded-lg">
+    <h1 className="text-4xl font-bold text-center ">Agent</h1>
   </div>
-  <div className="grid grid-cols-5 gap-x-16 gap-y-2 ">
+  <div className="grid grid-cols-5 gap-x-16 gap-y-2 bg-blue-highlight">
     {[...Array(20)].map((_, index) => (
       <div key={index} className="rounded-lg flex flex-col items-center justify-center">
         <div>
-          <img src="images/AgentBlack.svg" alt="Agent" className="w-10 h-auto mx-auto" />
-          <p className="text-center text-sm">#12DAC</p>
+          <img
+            src={index % 2 === 0 ? "images/AgentBlue.svg" : "images/AgentWhite.svg"}
+            alt="Agent"
+            className="w-10 h-auto mx-auto"
+          />
+          <p className={`text-center text-sm ${index % 2 === 0 ? "text-figma-figma1" : "text-white"}`}>
+            #12DAC
+          </p>
         </div>
       </div>
     ))}
