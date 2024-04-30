b7a24a98f862cfa791aa2af7e3cbeb4be5fe2688
Fernanda Osorio <A01026502@tec.mx>
2024-04-29 19:38:15 -0600

    Refactor ControlPanel layout and update Metrics component

--- src/app/ControlPanel/layout.tsx
+++ src/app/ControlPanel/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ReactNode } from "react";
 // import Home from '../NavBar'
+import SearchBar from "../searchBar";
 
 export default function ControlPanelLayout({
   children,
@@ -26,21 +27,24 @@ export default function ControlPanelLayout({
       <div className="h-screen">
         <div className="h-full flex ">
           <div className="flex-grow">{children}</div>
-          <div>
-              {/* Row for children and components */}
-                {/* Second column for Agent, Saturation, and Notif */}
-                <div className="flex h-4/5 justify-between p-4 px-20">
-                  <div>
-                    <div className="p-4">{Agent}</div>
-                    <div className="p-4">{Metrics}</div>
-                  </div>
-                  <div >
-                    <div className="p-4">{Saturation}</div>
-                    <div className="p-4">{Metrics2}</div>
-                  </div>
-                  <div className="p-4">{Notif}</div>
-                </div>
-            <div className="justify-between px-20">
+          <div className=" overflow-scroll  no-scrollbar">
+            {/* Row for children and components */}
+            {/* Second column for Agent, Saturation, and Notif */}
+            <div className="flex justify-end  px-16 pt-4">
+              <SearchBar />
+            </div>
+            <div className="flex h-4/5 justify-between pl-20 ">
+              <div>
+                <div className="p-4">{Agent}</div>
+                <div className="p-4">{Metrics}</div>
+              </div>
+              <div>
+                <div className="p-4">{Saturation}</div>
+                <div className="p-4">{Metrics2}</div>
+              </div>
+              <div className="p-4">{Notif}</div>
+            </div>
+            <div className="justify-between pl-20">
               <div className="p-4 ">{FU}</div>
             </div>
           </div>
