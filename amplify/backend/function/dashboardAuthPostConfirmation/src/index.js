7c992fcfbae7d8316a3e64ab34e062f4913031e3
Pablo Banzo <pridapablo@gmail.com>
2024-05-01 11:06:04 -0600

    bug fixes in preSignUp and postConf λ's

--- amplify/backend/function/dashboardAuthPostConfirmation/src/index.js
+++ amplify/backend/function/dashboardAuthPostConfirmation/src/index.js
@@ -5,36 +5,157 @@
 	ENV
 	REGION
 	STORAGE_DASHBOARDS3_BUCKETNAME
-Amplify Params - DO NOT EDIT *//**
- * @fileoverview
- *
- * This CloudFormation Trigger creates a handler which awaits the other handlers
- * specified in the `MODULES` env var, located at `./${MODULE}`.
- */
+Amplify Params - DO NOT EDIT */
+
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { ConnectClient, CreateUserCommand } = require("@aws-sdk/client-connect");
+const { sendAppSyncRequest } = require("./helpers/appSync.js");
+
+const REGION = process.env.AWS_REGION;
+const APPSYNCURL = process.env.API_DASHBOARD_GRAPHQLAPIENDPOINTOUTPUT;
+const GQLAPIKEY = process.env.API_DASHBOARD_GRAPHQLAPIKEYOUTPUT;
+const bucketName = process.env.STORAGE_DASHBOARDS3_BUCKETNAME;
+
+const getS3file = async () => {
+  const s3Client = new S3Client({ region: "us-east-1" });
+
+  const objectKey = "WalmartEmployeeList.csv";
+
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: objectKey,
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    const str = await response.Body.transformToString();
+
+    const tuples = str.split("\n").map((line) => {
+      const [email, role] = line.split(",");
+      return [email.trim(), role.trim()];
+    });
+
+    return tuples;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 /**
- * The names of modules to load are stored as a comma-delimited string in the
- * `MODULES` env var.
- */
-const moduleNames = process.env.MODULES.split(',');
-/**
- * The array of imported modules.
- */
-const modules = moduleNames.map((name) => require(`./${name}`));
+ @see https://docs.aws.amazon.com/connect/latest/APIReference/API_CreateUser.html
+ **/
+const createConnectUser = async (email, securityProfileId) => {
+  const connectClient = new ConnectClient({ region: "us-east-1" });
 
-/**
- * This async handler iterates over the given modules and awaits them.
- *
- * @see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html#nodejs-handler-async
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- *
- */
+  const inputConnect = {
+    // CreateUserRequest
+    Username: email, // required
+    Password: "Welcome2024", // required
+    PhoneConfig: {
+      // UserPhoneConfig
+      PhoneType: "SOFT_PHONE", // required
+    },
+    SecurityProfileIds: [
+      // SecurityProfileIds
+      securityProfileId, // required
+    ],
+    RoutingProfileId: "4eedec70-f00b-4398-b706-8acc23437010", // required
+    InstanceId: "96cb63e2-25a1-4014-8f3c-e3919415d597", // required
+    IdentityInfo: {
+      // UserIdentityInfo
+      Email: email, // required
+      FirstName: "John", // required
+      LastName: "Doe", // required
+    },
+  };
+
+  const commandConnect = new CreateUserCommand(inputConnect);
+  const responseConnect = await connectClient.send(commandConnect);
+
+  return responseConnect;
+};
+
+async function createAppSyncUser(email, role) {
+  const createUser = {
+    query: `mutation CreateUser($input: CreateUserInput!) {
+        createUser(input: $input) {
+            id
+            role
+            needsHelp
+        }
+      }`,
+    variables: {
+      input: {
+        id: email,
+        role: role,
+        needsHelp: true,
+      },
+    },
+  };
+
+  const createUserRes = await sendAppSyncRequest(
+    APPSYNCURL,
+    REGION,
+    "POST",
+    createUser,
+    GQLAPIKEY
+  );
+
+  if (!createUserRes.data[`createUser`]) {
+    console.error(`Could not create contact with id ${id}`);
+  }
+
+  return createUserRes.data[`createUser`];
+}
+
+const findUserByEmail = (email, usersCsv) => {
+  const user = usersCsv.find(([userEmail]) => userEmail === email);
+  return user ? user : null;
+};
+
 exports.handler = async (event, context) => {
-  /**
-   * Instead of naively iterating over all handlers, run them concurrently with
-   * `await Promise.all(...)`. This would otherwise just be determined by the
-   * order of names in the `MODULES` var.
-   */
-  await Promise.all(modules.map((module) => module.handler(event, context)));
-  return event;
+  const csv = await getS3file(event);
+
+  const user = findUserByEmail(event.request.userAttributes.email, csv);
+
+  if (user) {
+    let securityProfileId =
+      user[1] === "Agent"
+        ? "b393134a-079a-42b5-b7ce-c23d1ab88b83"
+        : "aeb2a576-2506-444a-9967-98891ba6e963";
+    let userRole = user[1] === "Agent" ? "AGENT" : "SUPERVISOR"; // Enum: AGENT, SUPERVISOR
+
+    try {
+      const responseDynamoDB = await createAppSyncUser(
+        event.request.userAttributes.email,
+        userRole
+      );
+      console.log(responseDynamoDB);
+    } catch (err) {
+      console.error(err.message);
+      if (err.message.includes("The conditional request failed")) {
+        throw new Error("User already exists in the system.");
+      } else {
+        throw new Error("Something went wrong with the DB user creation.");
+      }
+    }
+
+    try {
+      const responseConnect = await createConnectUser(
+        event.request.userAttributes.email,
+        securityProfileId
+      );
+      console.log(responseConnect);
+    } catch (err) {
+      console.error(err); // keep going even if the connect user creation fails
+    }
+
+    return event;
+  } else {
+    console.error("User not found in the list.");
+    throw new Error(
+      "Something went wrong with the user creation. Please contact your administrator."
+    );
+  }
 };
--- amplify/backend/function/dashboardAuthPreSignup/src/custom.js
+++ amplify/backend/function/dashboardAuthPreSignup/src/custom.js
@@ -5,10 +5,10 @@
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 
 const getS3file = async () => {
-  const client = new S3Client({region: 'us-east-1'});
+  const client = new S3Client({ region: "us-east-1" });
 
-  const bucketName = 'dashboard-s35ea37-staging';
-  const objectKey = 'WalmartEmployeeList.csv';
+  const bucketName = process.env.STORAGE_DASHBOARDS3_BUCKETNAME;
+  const objectKey = "WalmartEmployeeList.csv";
 
   const command = new GetObjectCommand({
     Bucket: bucketName,
@@ -20,20 +20,19 @@ const getS3file = async () => {
     // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
     const str = await response.Body.transformToString();
 
-    const tuples = str.split('\n').map(line => {
-      const [email, role] = line.split(',');
+    const tuples = str.split("\n").map((line) => {
+      const [email, role] = line.split(",");
       return [email.trim(), role.trim()];
     });
 
     return tuples;
-    
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 const findUserByEmail = (email, userData) => {
-  const user = userData.find(([userEmail,]) => userEmail === email);
+  const user = userData.find(([userEmail]) => userEmail === email);
   return user ? user : null;
 };
 
@@ -42,12 +41,11 @@ exports.handler = async (event, context) => {
 
   const user = findUserByEmail(event.request.userAttributes.email, data);
 
-
   if (user) {
     return event; // Return the role of the user
   } else {
-    throw new Error("Sign up from this email domain is not allowed.");  // Return null if user is not found
+    throw new Error(
+      ". Email not allowed, ask your admin to add you to the list."
+    );
   }
 };
-
-  
