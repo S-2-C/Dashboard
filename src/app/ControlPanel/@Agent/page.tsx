bfde8e5b24a99fd7680332140a0dedc5c00a8c4e
Fernanda Osorio <fernanda.osorio.arroyo@gmail.com>
2024-05-20 18:56:49 -0600

    added supervisors

--- src/app/ControlPanel/@Agent/page.tsx
+++ src/app/ControlPanel/@Agent/page.tsx
@@ -10,8 +10,6 @@ import {
   User,
 } from "@/API";
 
-
-
 export default function AgentSlot() {
   const [activeUsers, setActiveUsers] = useState<User[]>([]);
   const [alertUsers, setAlertUsers] = useState<User[]>([]);
@@ -42,28 +40,35 @@ export default function AgentSlot() {
   }, []);
 
   const combinedUsers = [
-    ...alertUsers.map(user => ({ ...user, status: 'alert' })),
-    ...activeUsers.map(user => ({ ...user, status: 'active' })),
-    ...offlineUsers.map(user => ({ ...user, status: 'offline' }))
+    ...alertUsers.map((user) => ({ ...user, status: "alert" })),
+    ...activeUsers.map((user) => ({ ...user, status: "active" })),
+    ...offlineUsers.map((user) => ({ ...user, status: "offline" })),
+    ...offlineSupervisors.map((user) => ({ ...user, status: "offlineSuper" })),
+
   ];
 
   const getUserProperties = (status: string) => {
     switch (status) {
-      case 'alert':
+      case "alert":
         return {
           imgSrc: "images/AgentRed.svg",
           textClass: "text-red-500",
         };
-      case 'active':
+      case "active":
         return {
           imgSrc: "images/AgentYellow.svg",
           textClass: "text-agenman-agenmanyellow",
         };
-      case 'offline':
+      case "offline":
         return {
           imgSrc: "images/AgentBlue.svg",
           textClass: "text-figma-figma1",
         };
+        case "offlineSuper":
+          return {
+            imgSrc: "images/AgentBlack.svg",
+            textClass: "text-black-500",
+          };
       default:
         return {
           imgSrc: "",
@@ -74,7 +79,7 @@ export default function AgentSlot() {
 
   return (
     <div
-      className="bg-blue-highlight rounded-lg p-8 shadow-md h-96 border-2 border-blue-800"
+      className="bg-blue-highlight rounded-lg p-8 shadow-md h-96 overflow-auto"
       style={{ display: "inline-block" }}
     >
       <div className="bg-blue-highlight rounded-lg">
@@ -82,7 +87,7 @@ export default function AgentSlot() {
           <h1 className="text-4xl font-bold text-center ">Agents</h1>
         </Link>
       </div>
-      <div className="h-full grid grid-cols-5 gap-x-16 gap-y-2 bg-blue-highlight p-4 overflow-y-auto">
+      <div className="h-full grid grid-cols-5 gap-x-16 gap-y-2 bg-blue-highlight p-4 overflow-y-auto  no-scrollbar">
       {combinedUsers.map((user, index) => {
           const { imgSrc, textClass } = getUserProperties(user.status);
           return (
@@ -95,7 +100,9 @@ export default function AgentSlot() {
                     alt="Agent"
                   />
                   <p className={`text-center text-sm ${textClass}`}>
-                    {user?.name ? user.name.split(" ")[0] : user.id.split("@")[0]}
+                    {user?.name
+                      ? user.name.split(" ")[0]
+                      : user.id.split("@")[0]}
                   </p>
                 </div>
               </Link>
