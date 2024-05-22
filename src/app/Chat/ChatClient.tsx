a89c3c0fd4dfebc90e0b7349494ceed9c72b9ea9
daniel-rdcz <danield_895@hotmail.com>
2024-05-21 23:25:18 -0600

    Avance

--- src/app/Chat/ChatClient.tsx
+++ src/app/Chat/ChatClient.tsx
@@ -29,8 +29,9 @@ export const ChatClient: React.FC<ChatClientProps> = ({
         <Flex direction="column" style={{ height: '100vh', width: '100%' }}>
             <Flex direction="row" style={{ height: '100%', backgroundColor: '#FFFFFF' }}>
                 <Flex direction="column" style={{ zIndex: 10 }}><Home /></Flex>
-                <Flex direction="column" style={{ flex: '1', marginLeft: '65px', padding: '10px', position: 'relative', zIndex: 5 }}>
-                    <Flex style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <Flex direction="column" style={{flex: '1', marginLeft: '65px', padding: '10px', position: 'relative', zIndex: 5 }}>
+                    <Text style={{ fontWeight: 600, fontSize: 25, marginLeft: '10px'}}>Messages</Text>
+                    <Flex style={{ width: '100%', display: 'flex', justifyContent: 'left' }}>
                         <StoryTypeMenu />
                     </Flex>
                     <View style={{ overflowY: 'scroll', marginBottom: '20px' }}>
--- src/app/Chat/Stories.tsx
+++ src/app/Chat/Stories.tsx
@@ -23,7 +23,7 @@ const Stories: React.FC = () => {
         } else {
             setShowLeft(false);
         }
-        if (storiesRef.current && storiesRef.current.scrollLeft === storiesRef.current.scrollWidth - storiesRef.current.clientWidth) {
+        if (storiesRef.current && storiesRef.current.scrollLeft > storiesRef.current.scrollWidth - storiesRef.current.clientWidth - 5) {
             setShowRight(false);
         } else {
             setShowRight(true);
@@ -31,19 +31,28 @@ const Stories: React.FC = () => {
     };
 
     return (
-        <div className='relative w-max'>
+        <div className='relative w-full'>
             <div 
                 onScroll={onScroll} 
                 ref={storiesRef} 
-                className="flex space-x-2 overflow-x-scroll bg-white p-2 scroll-smooth scrollbar-hide rounded-full"
-                style={{ border: '3px solid #057dc7', backgroundColor: '#e2f0f9', maxWidth: '48rem' }} // 48rem is slightly larger than max-w-lg (32rem)
+                className="flex space-x-2 overflow-x-scroll bg-white scroll-smooth scrollbar-hide rounded-full"
+                style={{ 
+                    backgroundColor: '#e2f0f9', 
+                    padding: '10px 25px 5px 25px', 
+                    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Sombra en el borde inferior
+                    maxWidth: '45vw'
+                }}
             >
                 {stories.map((story: Story) => (
-                    <Story 
-                        key={story.id}
-                        img={story.avatar}
-                        username={story.first_name + " " + story.last_name}
-                    />
+                    <div 
+                        key={story.id} 
+                        className="transition-transform transform hover:scale-105 hover:translate-y-2"
+                    >
+                        <Story 
+                            img={story.avatar}
+                            username={story.first_name + " " + story.last_name}
+                        />
+                    </div>
                 ))}
             </div>
             <div className='absolute top-0 p-4 w-full h-full flex justify-between z-10 items-center'>
@@ -59,3 +68,4 @@ const Stories: React.FC = () => {
 };
 
 export default Stories;
+ 
