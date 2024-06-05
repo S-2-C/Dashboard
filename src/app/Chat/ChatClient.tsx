bd3f3a2c8f19d3e42b4bb03c0ff7cc231b61265e
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-06-04 19:16:11 -0600

    Add usernames

--- src/app/Chat/ChatClient.tsx
+++ src/app/Chat/ChatClient.tsx
@@ -5,6 +5,8 @@ import StoryTypeMenu from './StoryTypeMenu';
 import ChatList from './ChatList'; // Import ChatList component
 import ChatWindow from './ChatWindow'; // Import ChatWindow component
 import members from './ChatListDummy.json'; // Import dummy data
+import { useEffect, useState } from 'react';
+import { fetchListUsers } from '../../fetching/fetchingDataFunctions';
 
 interface ChatClientProps {
     isConnected: boolean;
@@ -24,21 +26,33 @@ export const ChatClient: React.FC<ChatClientProps> = ({
     onDisconnect
 }) => {
 
+
+
     const handleSendMessage = (message: string) => {
         // Implement the logic to handle sending a message
     };
+    const [agents, setAgents] = useState<any[]>([]);
+
+
+
+    const fetchData = async () => {
+        const response = await fetchListUsers();
+        setAgents(response.data);
+        console.log("Agents", response.data);
+    };
+
 
     return (
         <Flex direction="column" style={{ height: '100vh', width: '100%' }}>
             <Flex direction="row" style={{ height: '100%', backgroundColor: '#FFFFFF' }}>
-                <Flex direction="column" style={{ zIndex: 10 }}><Home/></Flex>
+                <Flex direction="column" style={{ zIndex: 10 }}><Home /></Flex>
                 <Flex direction="column" style={{ flex: 1, marginLeft: '65px', padding: '30px', position: 'relative', zIndex: 5 }}>
                     <Text style={{ fontWeight: 600, fontSize: 25, marginLeft: '10px' }}>Messages</Text>
-                    <Text style={{fontWeight: 500, fontSize: 17, marginLeft: '10px'}}>Agents</Text>
-                    <Flex style={{ width: '100%', display: 'flex', justifyContent: 'space-between'}}>
-                        <Flex direction="column" style={{ width: '36vw', marginRight: '20px'}}>
+                    <Text style={{ fontWeight: 500, fontSize: 17, marginLeft: '10px' }}>Agents</Text>
+                    <Flex style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+                        <Flex direction="column" style={{ width: '36vw', marginRight: '20px' }}>
                             <StoryTypeMenu />
-                            <Text style={{fontWeight: 500, fontSize: 17, marginLeft: '10px'}}>Conversation</Text>
+                            <Text style={{ fontWeight: 500, fontSize: 17, marginLeft: '10px' }}>Conversation</Text>
                             <ChatList members={members} />
                         </Flex>
                         <ChatWindow chatRows={chatRows} onSendMessage={handleSendMessage} />
--- src/app/Chat/Stories.tsx
+++ src/app/Chat/Stories.tsx
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import stories from './StoriesDummy.json';
 import Story from './Story';
+import { useEffect, useState } from 'react';
+import { fetchListUsers } from '../../fetching/fetchingListAgent';
 
 interface Story {
     id: number;
@@ -11,34 +13,55 @@ interface Story {
 }
 
 const Stories: React.FC = () => {
+    const [agents, setAgents] = useState<any[]>([]);
+
     const storiesRef = useRef<HTMLDivElement>(null);
 
     const onScroll = () => {
         // No need to update button visibility anymore
     };
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetchListUsers();
+            setAgents(response.data);
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(agents, "agents");
+
+    // Set agent username to stories array
+    for (let i = 0; i < agents.length; i++) {
+        stories[i].first_name = agents[i].Username;
+        stories[i].id = agents[i].Id;
+    }
+    console.log(stories, "stories");
+
+
 
     return (
         <div className='relative w-full'>
-            <div 
-                onScroll={onScroll} 
-                ref={storiesRef} 
+            <div
+                onScroll={onScroll}
+                ref={storiesRef}
                 className="flex space-x-2 overflow-x-scroll scroll-smooth scrollbar-hide rounded-full"
-                style={{ 
-                    backgroundColor: '#ECECEC', 
-                    padding: '12px 20px 12px 20px', 
+                style={{
+                    backgroundColor: '#ECECEC',
+                    padding: '12px 20px 12px 20px',
                     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Sombra en el borde inferior
                     maxWidth: '100%',
                     overflowY: 'hidden' // Bloquea el desplazamiento vertical
                 }}
             >
                 {stories.map((story: Story) => (
-                    <div 
-                        key={story.id} 
+                    <div
+                        key={story.id}
                         className="transition-transform transform hover:scale-105 hover:translate-y-2"
                     >
-                        <Story 
+                        <Story
                             img={story.avatar}
-                            username={story.first_name + " " + story.last_name}
+                            username={story.first_name}
                         />
                     </div>
                 ))}
